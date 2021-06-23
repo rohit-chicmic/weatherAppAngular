b1845a0dc544b3228f2dcbab2e7f39b080924900
rohit-chicmic <rohit.kumar@chicmic.co.in>
2021-06-23 17:16:22 +0530

    new changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherAPI: {
+    baseUrl: 'https://api.openweathermap.org/data/2.5',
+    apiKey: 'b23b4e446c851c55c6ef7fcb1554dc8b'
+  }
 };
